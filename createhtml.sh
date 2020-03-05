50283fddec70cc93df4f5082d3590627a090e020
robindanzinger <3295716+robindanzinger@users.noreply.github.com>
2020-03-05 22:52:35 +0100

    make script more robust, support subfolders

--- createhtml.sh
+++ createhtml.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
 dest='predist/'
 pre='pre.html'
 post='post.html'
-for page in src/Pages/* 
-do 
-  filename=$dest${page##*/}
+for page in $(find ./src/Pages/ -type f -print)
+do
+  filename=$dest${page#./src/Pages/}
+  folder=${filename%/*}
+  mkdir -p $folder
   cat $pre > $filename
   cat $page >> $filename
   cat $post >> $filename
